a2afba0ad848b8ccdd800621628360409df15858
cosmiccapybara <37220709+cosmiccapybara@users.noreply.github.com>
2023-11-06 11:54:15 +1100

    Update matrix_for_binary_model.R

--- R/matrix_for_binary_model.R
+++ R/matrix_for_binary_model.R
@@ -24,7 +24,7 @@ matrix_binModel <- function(target_ct, data_path, qval_thresh, out_filename)
   directories <- list.dirs(path = data_path, full.names = T, recursive=F)
   celltypes <- basename(directories)
   
-  count_files <- list.files(path=data_path, full.names =T, recursive=T, pattern='fimo.tsv*')
+  counts_files <- list.files(path=data_path, full.names =T, recursive=T, pattern='fimo.tsv*')
   # Read results of motif search
   message(paste0("Reading input data from ",data_path,".\nThere are ", length(directories), " directories found."))
   suppressWarnings({
