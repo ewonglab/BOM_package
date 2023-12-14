f097c74b3c016e82149816f3375fe55912b606f9
cosmiccapybara <37220709+cosmiccapybara@users.noreply.github.com>
2023-12-14 18:09:34 +1100

    Update predict_binary.R

--- R/predict_binary.R
+++ R/predict_binary.R
@@ -32,109 +32,129 @@ add.missing.vars_xgb <- function(xgb.model, testSet)
 #' predict_binary(motifs = motif_counts, xgb_model = paste0(extdata_path, "/tutorial/motifs/Cardiomyocytes_vs_other.rds"))
 #' }
 #' @export
-predict_binary <- function(motifs, xgb_model, training_set = NULL, title=NULL)
+predict_binary <- function(motifs, xgb_model, celltype_prefix = NULL
+                           , pred = NULL, training_set = NULL, title=NULL)
 {
-	message("Reading classification model...")
+  message("Reading classification model...")
   
-	xgb <- readRDS(xgb_model)
+  xgb <- readRDS(xgb_model)
+
+  message("Saving XGBoost model in .bin format...")
+  xgboost::xgb.save(xgb, gsub(".rds", ".bin", xgb_model))
   
-	message("Saving XGBoost model in .bin format...")
-	xgboost::xgb.save(xgb, gsub(".rds", ".bin", xgb_model))
+  message(paste("Best tree:", xgb$best_iteration, "\n"))
   
-	message(paste("Best tree:", xgb$best_iteration, "\n"))
+  # Reading table of motif counts
   
-	# Reading table of motif counts
+  message("Reading motif counts matrix...")
+  counts.tab <- read.table(file = motifs, header =T, stringsAsFactors = F, sep = '\t')
+  counts.tab$celltype <- NULL
   
-	message("Reading motif counts matrix...")
-	counts.tab <- read.table(file = motifs, header =T, stringsAsFactors = F, sep = '\t')
-	counts.tab$celltype <- NULL
+  counts.tab.NAs <- sapply(counts.tab, function(x) sum(is.na(x)))
   
-	counts.tab.NAs <- sapply(counts.tab, function(x) sum(is.na(x)))
+  if(any(counts.tab.NAs) > 0){
+    warning("NAs present in input matrix...")  
+  }
   
-	if(any(counts.tab.NAs) > 0){
-		warning("NAs present in input matrix...")  
-	}
+  # Binary label as numeric
+  counts.tab$binary_celltype <- as.numeric(counts.tab$binary_celltype)
   
-	# Binary label as numeric
-	counts.tab$binary_celltype <- as.numeric(counts.tab$binary_celltype)
+  # Split dataset into training, validation and test sets
+  set.seed(123)
+  motifs_split <- rsample::initial_split(counts.tab, prop = .6)
+  motifs_train <- rsample::training(motifs_split)
+  motifs_test <- rsample::testing(motifs_split)
   
-	# Split dataset into training, validation and test sets
-	set.seed(123)
-	motifs_split <- rsample::initial_split(counts.tab, prop = .6)
-	motifs_train <- rsample::training(motifs_split)
-	motifs_test <- rsample::testing(motifs_split)
+  set.seed(123)
+  motifs_split2 <- rsample::initial_split(motifs_test, prop = .5)
+  motifs_val <- rsample::training(motifs_split2)
+  motifs_test <- rsample::testing(motifs_split2)
   
-	set.seed(123)
-	motifs_split2 <- rsample::initial_split(motifs_test, prop = .5)
-	motifs_val <- rsample::training(motifs_split2)
-	motifs_test <- rsample::testing(motifs_split2)
+  # Removing non-variable motifs from training set
+  motifs_train.sd <- apply(motifs_train, 2, sd)
+  motifs_train <- motifs_train[, names(which(motifs_train.sd != 0))]
   
-	# Removing non-variable motifs from training set
-	motifs_train.sd <- apply(motifs_train, 2, sd)
-	motifs_train <- motifs_train[, names(which(motifs_train.sd != 0))]
+  # Save training set if a file name is provided
+  if (is.null(training_set)) {
+    if(is.null(celltype_prefix)){
+      stop("No celltype_prefix provided.")
+    }
+    training_set <- paste0(celltype_prefix, "_vs_Others_train.txt")
+    message(paste("Saving training set to", training_set, "..."))
+    write.table(x = motifs_train, file = training_set, quote = FALSE, sep ='\t')
+  }else{
+    message(paste("Saving training set to", training_set, "..."))
+    write.table(x = motifs_train, file = training_set, quote = FALSE, sep ='\t')
+  }
   
-	# Save training set if a file name is provided
-	if (!is.null(training_set)) {
-		message(paste("Saving training set to", training_set, "..."))
-		write.table(x = motifs_train, file = training_set, quote = FALSE, sep ='\t')
-	}
+  motifs_test <- add.missing.vars_xgb(xgb, motifs_test)
+  test_labels <- motifs_test$binary_celltype
+  motifs_test <- motifs_test[,xgb$feature_names]  
   
-	motifs_test <- add.missing.vars_xgb(xgb, motifs_test)
-	test_labels <- motifs_test$binary_celltype
-	motifs_test <- motifs_test[,xgb$feature_names]  
+  set.seed(123)
+  y_pred <- predict(xgb, data.matrix(motifs_test), type="response")
+  predicted.class <- y_pred > 0.5 
+  predicted.class <- gsub("TRUE", 1, predicted.class)
+  predicted.class <- gsub("FALSE", 0, predicted.class)
   
-	set.seed(123)
-	y_pred <- predict(xgb, data.matrix(motifs_test), type="response")
-	predicted.class <- y_pred > 0.5 
-	predicted.class <- gsub("TRUE", 1, predicted.class)
-	predicted.class <- gsub("FALSE", 0, predicted.class)
-  
-	actual.vs.predicted <- data.frame(true_class = test_labels
+  actual.vs.predicted <- data.frame(true_class = test_labels
                                     , predicted_class = predicted.class
                                     , prob = y_pred
                                     , stringsAsFactors = F)
   
-	rownames(actual.vs.predicted) <- rownames(motifs_test)
+  rownames(actual.vs.predicted) <- rownames(motifs_test)
+  
+  if (is.null(pred)) {
+    if(is.null(celltype_prefix)){
+      stop("No celltype_prefix provided.")
+    }
+    pred <- paste0(celltype_prefix, "_vs_Others_pred.txt")
+    message(paste("Saving predictions to", pred, "..."))
+    write.table(x = actual.vs.predicted, file = pred, quote = FALSE, sep ='\t')
+  }else{
+    message(paste("Saving predictions to", pred, "..."))
+    write.table(x = actual.vs.predicted, file = pred, quote = FALSE, sep ='\t')
+  }
+  
+  message("Preparing ROC plots")
+  require(cvAUC)
+  require(pROC)
+  require(ggplot2)
   
   
-	message("Preparing ROC plots")
-	require(cvAUC)
-	require(pROC)
-	require(ggplot2)
-	
-	
-	roc_pred_tab <- roc(actual.vs.predicted$true_class, actual.vs.predicted$prob, direction="<")
-	roc_auc <- auc(roc_pred_tab)
+  roc_pred_tab <- roc(actual.vs.predicted$true_class, actual.vs.predicted$prob, direction="<")
+  roc_auc <- auc(roc_pred_tab)
   
-	rocs.list <- list(roc_pred_tab)
-	# Plot ROC curves
-	p <- ggroc(rocs.list, size = 1) + theme(panel.border = element_blank(), panel.grid.major = element_blank()
-                                        , panel.grid.minor = element_blank()#legend.position="none" 
-                                        , axis.line = element_line(colour = "black", size = 1)
-                                        , legend.title = element_blank()
-                                        , legend.key=element_blank()
-                                        , legend.position = "none"
-                                        , legend.key.width = unit(1.5,"cm")
-                                        , panel.background = element_blank()
-                                        , text = element_text(size=23)
-                                        , axis.text.x=element_text(colour="black")
-                                        , axis.text.y=element_text(colour="black")
-                                        , legend.text=element_text(size=24)
-                                        , axis.ticks = element_line(colour = "black")) +
-					guides(linetype = guide_legend(override.aes = list(size = 3))) +
-					geom_abline(slope=1, intercept = 1, linetype = "dashed", alpha=0.8, color = "grey") + coord_equal() +
-					scale_colour_manual(values=c("#B39EB5"), aesthetics = c("colour", "fill")) +
-					labs(y= "Sensitivity", x = "Specificity") + 
-					annotate("text", x = .5, y = .25,label = paste("AUC =", round(auc(roc_pred_tab), 2)))
+  rocs.list <- list(roc_pred_tab)
+  # Plot ROC curves
+  p <- ggroc(rocs.list, size = 1) + theme(panel.border = element_blank(), panel.grid.major = element_blank()
+                                          , panel.grid.minor = element_blank()#legend.position="none" 
+                                          , axis.line = element_line(colour = "black", size = 1)
+                                          , legend.title = element_blank()
+                                          , legend.key=element_blank()
+                                          , legend.position = "none"
+                                          , legend.key.width = unit(1.5,"cm")
+                                          , panel.background = element_blank()
+                                          , text = element_text(size=23)
+                                          , axis.text.x=element_text(colour="black")
+                                          , axis.text.y=element_text(colour="black")
+                                          , legend.text=element_text(size=24)
+                                          , axis.ticks = element_line(colour = "black")) +
+    guides(linetype = guide_legend(override.aes = list(size = 3))) +
+    geom_abline(slope=1, intercept = 1, linetype = "dashed", alpha=0.8, color = "grey") + coord_equal() +
+    scale_colour_manual(values=c("#B39EB5"), aesthetics = c("colour", "fill")) +
+    labs(y= "Sensitivity", x = "Specificity") + 
+    annotate("text", x = .5, y = .25,label = paste("AUC =", round(auc(roc_pred_tab), 2)))
   
-	if (! is.null(title))
-	{	p <- p + ggtitle(title)
-	}
-
-	return(p)
+  if (! is.null(title))
+  {	p <- p + ggtitle(title)
+  }
+  
+  return(p)
 }
 
-
+			   
+			   
 #############################################################
 #' predict_binary_multi
 #' @description Wrapper function for predict_binary. Will run 
@@ -158,46 +178,48 @@ predict_binary <- function(motifs, xgb_model, training_set = NULL, title=NULL)
 #'
 #' @export
 #'
-predict_binary_multi <- function(inputMotif_dir=NULL, inputXGB_dir=NULL, outputTrain_dir=NULL, ncol = 3, nrow = 3, 
-				outputFile =NULL, width=12, height=12)
+predict_binary_multi <- function(inputMotif_dir=NULL, inputXGB_dir=NULL, outputTrain_dir=NULL
+                                 , pred_dir = NULL, ncol = 3, nrow = 3, 
+                                 outputFile =NULL, width=12, height=12)
 {
-	if (is.null(outputFile ))
-	{ 	warning("Please provide output file name") 
-		return(-1)
-	}
-        require(cowplot)	
-	
-	fl.motifs <- list.files(path=inputMotif_dir, pattern="*_vs_Others.txt")
-	fl.xgb    <- list.files(path=inputXGB_dir, pattern="*_vs_Others.rds")
-	
-	# Identify which entries have both a 
-	candidates <- c(gsub(pattern="\\.txt$", replacement = '', x= fl.motifs),gsub(pattern="\\.rds$", replacement='', x= fl.xgb))
-	candidates <- candidates[which(duplicated(candidates))]
-	
-	allPlots <- list()
-	
-	for (i in 1: length(candidates))
-	{	message(paste0( "Preparing ", candidates[i], " predict binary output") )	
-		allPlots[[i]] <- suppressMessages(
-							predict_binary(motifs = paste0(inputMotif_dir,  "/", candidates[i], ".txt"),
-							xgb_model = paste0(inputXGB_dir,    "/", candidates[i], '.rds'), 
-						training_set  = paste0(outputTrain_dir, "/", candidates[i], '_train.txt'),
-						title=gsub(pattern='_vs_Others', replacement='', x=candidates[i])
-							))
-	}
-	nsamples <- ncol * nrow
-	currentSample <- 1
-	pages <- ceiling(length(candidates)/nsamples)
-	
-	
-	pdf(outputFile, width=width, height=height)
-	for(i in 1:pages)
-	{	
-		print(plot_grid(plotlist=allPlots[currentSample:(nsamples*i)], ncol=ncol, nrow=nrow))
-		currentSample <- currentSample + nsamples
-	}
-	dev.off()
-		
-	
-	
+  if (is.null(outputFile ))
+  { 	warning("Please provide output file name") 
+    return(-1)
+  }
+  require(cowplot)	
+  
+  fl.motifs <- list.files(path=inputMotif_dir, pattern="*_vs_Others.txt")
+  fl.xgb    <- list.files(path=inputXGB_dir, pattern="*_vs_Others.rds")
+  
+  # Identify which entries have both a 
+  candidates <- c(gsub(pattern="\\.txt$", replacement = '', x= fl.motifs),gsub(pattern="\\.rds$", replacement='', x= fl.xgb))
+  candidates <- candidates[which(duplicated(candidates))]
+  
+  allPlots <- list()
+  
+  for (i in 1: length(candidates))
+  {	message(paste0( "Preparing ", candidates[i], " predict binary output") )	
+    allPlots[[i]] <- suppressMessages(
+      predict_binary(motifs = paste0(inputMotif_dir,  "/", candidates[i], ".txt"),
+                     xgb_model = paste0(inputXGB_dir,    "/", candidates[i], '.rds'), 
+                     training_set  = paste0(outputTrain_dir, "/", candidates[i], '_train.txt'),
+                     pred = paste0(pred_dir, "/", candidates[i], "_pred.txt"),
+                     title=gsub(pattern='_vs_Others', replacement='', x=candidates[i])
+      ))
+  }
+  nsamples <- ncol * nrow
+  currentSample <- 1
+  pages <- ceiling(length(candidates)/nsamples)
+  
+  
+  pdf(outputFile, width=width, height=height)
+  for(i in 1:pages)
+  {	
+    print(plot_grid(plotlist=allPlots[currentSample:(nsamples*i)], ncol=ncol, nrow=nrow))
+    currentSample <- currentSample + nsamples
+  }
+  dev.off()
+  
+  
+  
 }
