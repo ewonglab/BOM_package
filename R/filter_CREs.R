fcfc540c7e95b8c10ad21f7e2c3ba22fb7bfdc72
Paola Cornejo <37220709+cosmiccapybara@users.noreply.github.com>
2024-12-05 16:00:34 +1100

    Update filter_CREs.R

--- R/filter_CREs.R
+++ R/filter_CREs.R
@@ -4,20 +4,17 @@
 #' @param x data frame of genomic coordinates. BED file format. 
 #' @param N Number of base pairs - used to extend coordinates stored in x.
 #' @param chom_sizes  dataframe of chromosome sizes
-#' 
+#'
 adjust_CREs <- function(x, N, chrom_sizes){
   
-  x$width <- with(x, V3-V2)
-  #x$centre <- with(x, V2 + (width/2))
+  x$width <- with(x, V3 - V2)
   x$centre <- with(x, V2 + round(width/2))
-  #x$start <- ceiling(x$centre - N/2)
   x$start <- round(x$centre - N/2)
-  #x$end <- ceiling(x$centre + N/2)
   x$end <- round(x$centre + N/2)
 
-  # add chromosome size
+  # Add chromosome size
   x$ord <- 1:nrow(x)
-  x <- merge(x, chrom_sizes, by.x="V1", by.y="chr")
+  x <- merge(x, chrom_sizes, by.x = "V1", by.y = "chr")
 
   # Look for CREs at the extremes of chromosomes
   if(any(x$start < 0) | any(x$end > x$chr_size)){
@@ -33,10 +30,10 @@ adjust_CREs <- function(x, N, chrom_sizes){
     }
   }
   
-  #return to original order
+  # Return to original order
   x <- x[with(x, order(ord)), ]
-  x <- x[,colnames(x)[!colnames(x) %in% c("chr_size", "centre", "width", "V2", "V3", "ord")]]
-  x <- x[,c("V1", "start", "end", setdiff(colnames(x), c("V1", "start", "end")))]
+  x <- x[, colnames(x)[!colnames(x) %in% c("chr_size", "centre", "width", "V2", "V3", "ord")]]
+  x <- x[, c("V1", "start", "end", setdiff(colnames(x), c("V1", "start", "end")))]
   colnames(x)[2:3] <- c("V2", "V3")
   return(x)
   
@@ -67,13 +64,13 @@ adjust_CREs <- function(x, N, chrom_sizes){
 #'
 #' @examples 
 #' \dontrun{
-#' extdata_path <- system.file("extdata",package = "BagOfMotifs")
+#' extdata_path <- system.file("extdata", package = "BagOfMotifs")
 #' input.bed <- paste0(extdata_path, "/tutorial/mouseE8.25_peaks.bed")
-#' annot.file <- paste0(extdata_path,"/Mus_musculus.GRCm38.92.gtf.gz")
-#' mouseChrSizes <- paste0(extdata_path,"/mouse_sizes_primary_genome_tab.txt")
-#' 
-#' 
-#'  filterCRE(inputBedFile = input.bed, annotFile = annot.file, 
+#' annot.file <- paste0(extdata_path, "/Mus_musculus.GRCm38.92.gtf.gz")
+#' mouseChrSizes <- paste0(extdata_path, "/mouse_sizes_primary_genome_tab.txt")
+#'
+#'
+#' filterCRE(inputBedFile = input.bed, annotFile = annot.file, 
 #'                     remove_proximal = TRUE,  non_exonic = TRUE,
 #' 					   u =  1000, d=1000, nbp=500,
 #'                     chrSizesFile = mouseChrSizes, 
@@ -89,46 +86,42 @@ filterCREs <- function(inputBedFile = NULL,
                        keep_proximal = FALSE,
                        remove_proximal = FALSE,
                        non_exonic = FALSE,
-                       out_bed = NULL, inputBedZeroBased=TRUE,
+                       out_bed = NULL, inputBedZeroBased = TRUE,
                        celloutputDir = NULL, minCellPercent = 1,
-                       ovr_dir = FALSE)
-{
-  if (is.null(celloutputDir))
-  {
+                       ovr_dir = FALSE){
+  if (is.null(celloutputDir)){
     stop("celloutputDir is not set.")
-  }
-  else if(file.exists(celloutputDir) & !ovr_dir){
+  } else if (file.exists(celloutputDir) & !ovr_dir){
       stop(paste("The directory", celloutputDir, "already exist. Set ovr_dir to TRUE"))
-  }else{
+  } else {
       suppressWarnings(dir.create(celloutputDir))
   }
   
   message("Reading CREs...\n")
-  addToBed = 0
-  if (inputBedZeroBased == TRUE)
-  { addToBed = 1 } 
+  addToBed <- 0
+  if (inputBedZeroBased == TRUE){
+	  addToBed <- 1
+  } 
   
-  cres <- read.table(file = inputBedFile, header = F, stringsAsFactors = F, sep = '\t')
+  cres <- read.table(file = inputBedFile, header = FALSE, stringsAsFactors = FALSE, sep = '\t')
   
   # Clean up 4th column by removing spaces an slashes
   cres$V4 <- sub("/", "_", cres$V4)
   cres$V4 <- sub(" ", "_", cres$V4)
   
-  cres_gr <- with(cres, GenomicRanges::GRanges(V1, IRanges::IRanges(V2+addToBed, V3), cellType=cres$V4))
+  cres_gr <- with(cres, GenomicRanges::GRanges(V1, IRanges::IRanges(V2 + addToBed, V3), cellType = cres$V4))
   
-  if(keep_proximal | remove_proximal | non_exonic)
-  {
+  if(keep_proximal | remove_proximal | non_exonic){
     
     message("Reading genome annotation...\n")
     txdb_obj <- suppressWarnings(GenomicFeatures::makeTxDbFromGFF(file = annotFile, format = "gtf"))
     
     matchingChromosomes <- intersect(GenomeInfoDb::seqlevels(txdb_obj), GenomeInfoDb::seqlevels(cres_gr))
-    if (length(matchingChromosomes) == 0)
-    { 	# No chromosomes align - Change style and try again
+    if (length(matchingChromosomes) == 0){
+      # No chromosomes align - Change style and try again
       GenomeInfoDb::seqlevelsStyle(cres_gr) <- "NCBI"
       matchingChromosomes <- intersect(GenomeInfoDb::seqlevels(txdb_obj), GenomeInfoDb::seqlevels(cres_gr))
-      if (length(matchingChromosomes) == 0)
-      {
+      if (length(matchingChromosomes) == 0){
         warning("Seqence levels do not match to reference")
         warning(seqlevels(txdb_obj))
         warning(seqlevels(cres_gr))
@@ -158,6 +151,7 @@ filterCREs <- function(inputBedFile = NULL,
         stop(paste("No remaining CREs"))
       }
     }
+	  
     if(remove_proximal){
       message("Removing proximal regions to TSSs...\n")
       proximal <- GenomicFeatures::promoters(x = txdb_obj, upstream = u, downstream = d)
@@ -169,11 +163,9 @@ filterCREs <- function(inputBedFile = NULL,
     }
   }
   
-  #if((u == NULL & d != NULL) | (d == NULL & u != NULL)){
-  #  stop(paste("u AND d should be provided to adjust CREs"))
   if (exists("nbp") && !is.null(nbp)) {
     cat("Reading chromosome sizes...\n")
-    chrom_sizes <- read.table(file = chrSizesFile, header = F, stringsAsFactors = F, sep ='\t')
+    chrom_sizes <- read.table(file = chrSizesFile, header = F, stringsAsFactors = F, sep = "\t")
     colnames(chrom_sizes) <- c("chr", "chr_size")
     
     # Adjust CREs
@@ -183,7 +175,7 @@ filterCREs <- function(inputBedFile = NULL,
 	# Instead have added GenomicRanges resize and trim functions that will complete this process.
 	#cres <- adjust_CREs(cres, nbp, chrom_sizes)  
 	
-	cres_gr <- GenomicRanges::resize(cres_gr, width=nbp, fix="center")
+	cres_gr <- GenomicRanges::resize(cres_gr, width = nbp, fix = "center")
 
     
     idx <- which(chrom_sizes$chr %in% names(GenomeInfoDb::seqlengths(cres_gr)))
@@ -193,13 +185,11 @@ filterCREs <- function(inputBedFile = NULL,
       cres_gr <- IRanges::trim(cres_gr)
 	  # remove occurances that do not have a significant width
 	  idx.small <- which(GenomicRanges::width(cres_gr) < nbp)
-	  if (length(idx.small) > 0)
-	  {	message(paste0("Removing ", length(idx.small), " regions as they were truncated to a length smaller than ", nbp, " nt (i.e. were located near the edge of a chromosome)"))
-		cres_gr <- cres_gr[idx.small * -1]
+	  if (length(idx.small) > 0){
+		  message(paste0("Removing ", length(idx.small), " regions as they were truncated to a length smaller than ", nbp, " nt (i.e. were located near the edge of a chromosome)"))
+		  cres_gr <- cres_gr[idx.small * -1]
 	  }
-    }
-    else 
-    {	
+    } else {	
       warning("Cannot check out of bound granges as chromosome names between peaks and reference did not match")
     }
     
@@ -209,50 +199,47 @@ filterCREs <- function(inputBedFile = NULL,
   if(length(cres_gr) == 0){
     stop(paste("No remaining CREs after applying filters."))
   }	
-  cres <- as.data.frame(cres_gr)[,c('seqnames','start','end','cellType')] 
+  cres <- as.data.frame(cres_gr)[, c("seqnames","start", "end", "cellType")] 
   
   
   # Save filtered regions
-  if (! is.null(out_bed))
-  {   message(paste0("Saving ",  length(cres_gr) ," CREs...\n"))
-
-		write.table(x = cres, file = out_bed, quote = F, col.names = F
-              , row.names = F, sep ='\t')
+  if (! is.null(out_bed)){
+	  message(paste0("Saving ", length(cres_gr), " CREs...\n"))
+	  write.table(x = cres, file = out_bed, quote = FALSE, col.names = FALSE
+		      , row.names = FALSE, sep = "\t")
   }
   
   
   
-  if (! is.null(celloutputDir))
-  {	# Generate BED files for each cell type
-    # split into bed files
-    message("Preparing output directories and files so that everything is set for motif searching")
-    colnames(cres) <- c('chrom', 'start','end', 'cellType')
+  if (! is.null(celloutputDir)){
+	  # Generate BED files for each cell type
+	  # split into bed files
+	  message("Preparing output directories and files so that everything is set for motif searching")
+	  colnames(cres) <- c("chrom", "start", "end", "cellType")
     
     # Filter out low proportion of cells
     cellnumbers <- table(cres$cellType)
-    idx.toRemove <- which(cellnumbers/sum(cellnumbers) <  (minCellPercent/100) ) 
+    idx.toRemove <- which(cellnumbers / sum(cellnumbers) <  (minCellPercent / 100) ) 
     idx.toRemove <- names(cellnumbers)[idx.toRemove]
-    if (length(idx.toRemove) > 0)
-    {
+	  
+    if (length(idx.toRemove) > 0){
       cres <- cres[!cres$cellType %in% idx.toRemove, ]
-      message(paste0("\nThe following cell(s) represent less than ",minCellPercent,
+      message(paste0("\nThe following cell(s) represent less than ", minCellPercent,
                      "% of all cells.", 
                      "\nThis is the predefined cutoff as defined by parameter minCellPercent.",
                      "\nOutputs for following cell(s) will therefore not be generated:\n", 
-                     paste0(idx.toRemove, collapse="\n"),"\n"))
+                     paste0(idx.toRemove, collapse = "\n"), "\n"))
     }
     
  
     
-    
-    for(i in unique(cres$cellType))
-    {
-      fn <- paste0(celloutputDir, "/", gsub(" ", "_", i), ".bed")
-      write.table(x = cres[cres$cellType == i, ], file = fn, 
-                  quote = F, col.names = F, row.names = F, sep ='\t')
+    for(i in unique(cres$cellType)){
+	    fn <- paste0(celloutputDir, "/", gsub(" ", "_", i), ".bed")
+	    write.table(x = cres[cres$cellType == i, ], file = fn, 
+			quote = F, col.names = F, row.names = F, sep = "\t")
     }
-    cellNames <- paste0(gsub(" ", "_", unique(cres$cellType)),".bed", collapse="\n")
-    message(paste0("The following files have been prepared: \n",cellNames))
+    cellNames <- paste0(gsub(" ", "_", unique(cres$cellType)), ".bed", collapse = "\n")
+    message(paste0("The following files have been prepared: \n", cellNames))
   }
   
   
@@ -270,13 +257,12 @@ filterCREs <- function(inputBedFile = NULL,
 #' @param outputFileName Output file name. Default to "out.bed".
 #' @param removeMultiAnnotatedPeaks Boolean (default TRUE). Whether to remove CREs annotated to multiple cell types/states.
 #' @param removeUnnanotatedPeaks Boolean (default TRUE). Whether to remove CREs not annotated to a cell type/state.
-
 #' @examples 
 #' \dontrun{
 #' extdata_path <- system.file("extdata",package = "BagOfMotifs")
-#' filename <- paste0(extdata_path,'/Pijuan_etal_table_S6.csv.gz')
-#' 
-#' textToBed(inputTextFile = filename, outputFileName =  'mouseE8.25_peaks_filt.bed')
+#' filename <- paste0(extdata_path,"/Pijuan_etal_table_S6.csv.gz")
+#'
+#' textToBed(inputTextFile = filename, outputFileName =  "mouseE8.25_peaks_filt.bed")
 #' }
 #'
 #' @export
@@ -287,47 +273,43 @@ textToBED <- function(inputTextFile = NULL,
                       sep = ",",
                       outputFileName = "out.bed",
                       removeMultiAnnotatedPeaks = TRUE,
-                      removeUnnanotatedPeaks = TRUE
-)
-{
+                      removeUnnanotatedPeaks = TRUE){
   cnameLength <- length(inputcolnames)
-  if (cnameLength != 4)
-  { error(paste0("Only ", cnameLength, " column names provided. Must be 4 column names defined.") )
+  if (cnameLength != 4){
+	  error(paste0("Only ", cnameLength, " column names provided. Must be 4 column names defined.") )
   }
   
-  txtData <- read.table(file = inputTextFile, header = header, sep=sep, stringsAsFactors = F)
+  txtData <- read.table(file = inputTextFile, header = header, sep = sep, stringsAsFactors = F)
   
-  if (header == FALSE)
-  {	colnames(txtData)[1:4] <- inputcolnames
+  if (header == FALSE){
+	  colnames(txtData)[1:4] <- inputcolnames
   }
-  message("Input text file has ", nrow(txtData)," entries")
+  message("Input text file has ", nrow(txtData), " entries")
   
   
   
-  if (removeUnnanotatedPeaks)
-  {# removing all the peaks that were not annotated to a cell type
-    message("Removing ",length(which(is.na(txtData[,inputcolnames[4]])))," entries that are not annotated with cell type")
-    txtData <- txtData[!is.na(txtData[,inputcolnames[4]]),]  # celltype_specificity
+  if (removeUnnanotatedPeaks){
+	  # removing all the peaks that were not annotated to a cell type
+	  message("Removing ", length(which(is.na(txtData[,inputcolnames[4]]))), " entries that are not annotated with cell type")
+	  txtData <- txtData[!is.na(txtData[, inputcolnames[4]]), ]  # celltype_specificity
   }
   
-  if(removeMultiAnnotatedPeaks)
-  {		# remove peaks annotated to multiple cell types
-    idx <- paste(txtData[,inputcolnames[1]]
-                 , paste(txtData[,inputcolnames[2]]
-                         , txtData[,inputcolnames[3]], sep = "-")
-                 , sep = ':')
+  if(removeMultiAnnotatedPeaks){
+	  # remove peaks annotated to multiple cell types
+	  idx <- paste(txtData[,inputcolnames[1]]
+		       , paste(txtData[,inputcolnames[2]]
+			       , txtData[,inputcolnames[3]], sep = "-")
+		       , sep = ":")
     dup_idx <- (idx[duplicated(idx)])
-    message("Removing ",length(dup_idx)," unique entries annotated to multiple cell types")
+    message("Removing ", length(dup_idx), " unique entries annotated to multiple cell types")
     remove_idx <- which(idx %in% dup_idx)
-    txtData <- txtData[-remove_idx,]
+    txtData <- txtData[-remove_idx, ]
   }
   
-  message("Creating bed file with ",nrow(txtData), " entries")
+  message("Creating bed file with ", nrow(txtData), " entries")
   
   # Save processed peak into a bed file
-  
-  write.table(x = txtData[,inputcolnames], file = outputFileName, col.names = F, row.names = F, quote = F, sep = '\t')
-  
+  write.table(x = txtData[, inputcolnames], file = outputFileName, col.names = F, row.names = F, quote = F, sep = "\t")
   
 }
 
