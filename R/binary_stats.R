e7be252b87bbce3d455205fe1f90eeced17c9089
Paola Cornejo <37220709+cosmiccapybara@users.noreply.github.com>
2024-12-05 15:46:25 +1100

    Update binary_stats.R

--- R/binary_stats.R
+++ R/binary_stats.R
@@ -25,16 +25,15 @@ MCC <- function(TP, TN, FP, FN){
 #'
 #' extdata_path <- system.file("extdata",package = "BagOfMotifs")
 #' binPredictions <- paste0(extdata_path, "/tutorial/Cardiomyocytes_vs_other_pred.txt")
-#' 
-#' 
+#'
+#'
 #' binaryStats(inputFile = binPredictions)
-#
+#'
 #' @export
-binaryStats <- function(inputFile)
-{
+binaryStats <- function(inputFile){
   message("Reading predicted values...\n")
   
-  pred_tab <- read.table(file = inputFile, header = T, stringsAsFactors = F)
+  pred_tab <- read.table(file = inputFile, header = TRUE, stringsAsFactors = FALSE)
   TP <- nrow(pred_tab[pred_tab$true_class == 1 & pred_tab$predicted_class == 1, ])
   TN <- nrow(pred_tab[pred_tab$true_class == 0 & pred_tab$predicted_class == 0, ])
   FP <- nrow(pred_tab[pred_tab$true_class == 0 & pred_tab$predicted_class == 1, ])
@@ -42,18 +41,18 @@ binaryStats <- function(inputFile)
 
   recall <- TP/(TP + FN)
   precision <- TP/(TP + FP)
-  f1 <- 2*((precision*recall)/(precision+recall))
+  f1 <- 2 * ((precision * recall) / (precision + recall))
   pred_tab$true_class <- factor(pred_tab$true_class, levels = c(0,1))
   
   pr_auc_val <- yardstick::pr_auc(pred_tab, truth = true_class, prob
                                   , event_level = "second", estimator = "binary")
   mcc <- MCC(TP, TN, FP, FN)
-  Accuracy <- nrow(pred_tab[pred_tab$predicted_class==pred_tab$true_class,])/nrow(pred_tab)
+  Accuracy <- nrow(pred_tab[pred_tab$predicted_class == pred_tab$true_class, ]) / nrow(pred_tab)
   mcc <- MCC(TP, TN, FP, FN)
   
   
-  auROC <- round(cvAUC::AUC(pred_tab$prob, pred_tab$true_class),4)
-  pr_auc_val <- round(pr_auc_val$.estimate,4)
+  auROC <- round(cvAUC::AUC(pred_tab$prob, pred_tab$true_class), 4)
+  pr_auc_val <- round(pr_auc_val$.estimate, 4)
   Accuracy <- round(Accuracy, 4)
   f1 <- round(f1, 4)
   recall <- round(recall, 4)
@@ -62,12 +61,12 @@ binaryStats <- function(inputFile)
   
   ## Print binary prediction statistics
   message("Prediction statistics:")
-  cat(paste("auROC:", auROC, '\n'))
-  cat(paste("auPR:", pr_auc_val, '\n'))
-  cat(paste("Accuracy:", Accuracy, '\n'))
-  cat(paste("F1 score:", f1, '\n'))
-  cat(paste("Recall:", recall, '\n'))
-  cat(paste("Precision:", precision, '\n'))
-  cat(paste("MCC:", mcc, '\n'))
+  cat(paste("auROC:", auROC, "\n"))
+  cat(paste("auPR:", pr_auc_val, "\n"))
+  cat(paste("Accuracy:", Accuracy, "\n"))
+  cat(paste("F1 score:", f1, "\n"))
+  cat(paste("Recall:", recall, "\n"))
+  cat(paste("Precision:", precision, "\n"))
+  cat(paste("MCC:", mcc, "\n"))
   
 }
