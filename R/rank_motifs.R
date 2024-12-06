c9007ef824f09937ac5852edbe29468fffed5d8b
Paola Cornejo <37220709+cosmiccapybara@users.noreply.github.com>
2024-12-06 12:37:12 +1100

    Update rank_motifs.R

--- R/rank_motifs.R
+++ R/rank_motifs.R
@@ -1,28 +1,28 @@
 #############################################################
 #' rankMotifs
-#' @description Function to rank motifs by the sum or mean of absolute SHAP scores.  
-#' 
+#' @description Function to rank motifs by the sum or mean of absolute SHAP scores.
+#'
 #' @param shap_file  Path to the file containing SHAP scores
 #' @param out_file File name to output the motifs ranked by SHAP (default to "ranked_motifs")
 #' @param rank_type Rank type. Either 'sum' or 'mean'
-#' 
-#' @examples 
+#'
+#' @examples
 #'
 #' \dontrun{
 #' extdata_path <- system.file("extdata",package = "BagOfMotifs")
 #' binPredictions <- paste0(extdata_path, "/tutorial/Cardiomyocytes_vs_other_SHAP.txt")
-#' 
-#' 
+#'
+#'
 #' rankMotifs(shap_file = binPredictions, out_file = "ranked_motifs", type = "sum")
 #' }
 #' @export
 rankMotifs <- function(shap_file, type, out_file){
-  
+
     if(!type %in% c("sum", "mean")){
     stop(paste("Invalid ranking type:", type))
     }
   message("Reading SHAP values...\n")
-  shap <- read.table(file = shap_file, header = T, stringsAsFactors = F, sep ='\t', row.names = 1)
+  shap <- read.table(file = shap_file, header = TRUE, stringsAsFactors = FALSE, sep ='\t', row.names = 1)
   
   if(type == "sum"){
     shap_per_motif <- apply(shap, 2, function(x) sum(abs(x)))
@@ -43,6 +43,5 @@ rankMotifs <- function(shap_file, type, out_file){
   
   # return(shap_per_motif)
   message("Saving ranked motifs...\n")
-  write.table(x = shap_per_motif, file = out_file, quote = F)
-  
+  write.table(x = shap_per_motif, file = out_file, quote = FALSE) 
 }
